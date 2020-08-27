3fdf9ed8b59e7b90a6c8b433e3d855879eacbbab
VijaySharma19 <vijay29042002@gmail.com>
2020-08-28 01:01:25 +0530

    database moved to postgress
    Signed-off-by: VijaySharma19 <vijay29042002@gmail.com>

--- src/db/models.js
+++ src/db/models.js
@@ -1,11 +1,17 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize({
-    dialect : 'mysql',
-    username : 'cbsocialuser',
-    password : 'cbsocialpass',
-    database : 'cbsocialmediadb'
-})
+let db;
+if(process.env.DATABASE_URL){
+    db=new Sequelize("postgres://kbhisvzfubuycn:ddea6ae023c0a38a7615484d71fb2b3ac38b94d3908e8f43a970f2bf49b60385@ec2-54-91-178-234.compute-1.amazonaws.com:5432/d52nmqcd42daq5")
+}
+else{
+    db = new Sequelize({
+        dialect : 'mysql',
+        username : 'cbsocialuser',
+        password : 'cbsocialpass',
+        database : 'cbsocialmediadb'
+    })
+}
 
 const COL_ID_DEF = {
     type : Sequelize.DataTypes.INTEGER,
